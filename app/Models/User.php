da036d1cbfbdd72616e7a3482db48aff0f7c1e6d
Steward Lumowa <stewardyohanes29@gmail.com>
2024-04-07 02:50:43 +0800

    ready to deploy

--- app/Models/User.php
+++ app/Models/User.php
@@ -11,10 +11,19 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 use Ramsey\Uuid\Uuid;
+use Filament\Models\Contracts\FilamentUser;
+use Filament\Panel;
 
-class User extends Authenticatable
+
+
+class User extends Authenticatable implements FilamentUser
 {
   use HasApiTokens, HasFactory, Notifiable;
+
+  public function canAccessPanel(Panel $panel): bool
+  {
+    return str_ends_with($this->username, '@bagopsresminahasa.online');
+  }
 
   public function isAdmin()
   {
