c04c76111ef741af9c4fde5a5680f4eae78d1194
Steward Lumowa <stewardyohanes29@gmail.com>
2024-04-07 04:52:18 +0800

    deploy fixing 2

--- app/Models/User.php
+++ app/Models/User.php
@@ -21,7 +21,7 @@ class User extends Authenticatable implements FilamentUser
   use HasApiTokens, HasFactory, Notifiable;
   public function canAccessPanel(Panel $panel): bool
   {
-    return str_ends_with($this->username, '@bagopsresminahasa.online');
+    return true;
   }
 
   public function isAdmin()
