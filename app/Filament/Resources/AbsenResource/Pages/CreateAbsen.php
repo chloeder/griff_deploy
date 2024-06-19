fe3ded520d8f2bb2d729480feba0d187e28c1cf6
Steward Lumowa <stewardyohanes29@gmail.com>
2024-06-19 15:42:35 +0800

    update absen2

--- app/Filament/Resources/AbsenResource/Pages/CreateAbsen.php
+++ app/Filament/Resources/AbsenResource/Pages/CreateAbsen.php
@@ -27,21 +27,15 @@ class CreateAbsen extends CreateRecord
         ->send();
       $this->halt();
     } else {
-      if ($data['keterangan_absen'] === 'Hadir') {
-        $data['tanggal_masuk'] = Carbon::now()->format('Y-m-d');
-        $data['waktu_masuk'] = Carbon::now()->format('H:i:s');
-      } elseif ($data['keterangan_absen'] === 'Izin') {
-        $data['lokasi_masuk'] = null;
-        $data['tanggal_masuk'] = null;
-        $data['waktu_masuk'] = null;
-      } elseif ($data['keterangan_absen'] === 'Sakit') {
-        $data['lokasi_masuk'] = null;
-        $data['tanggal_masuk'] = null;
-        $data['waktu_masuk'] = null;
-      } else {
-        $data['lokasi_masuk'] = null;
-        $data['tanggal_masuk'] = null;
-        $data['waktu_masuk'] = null;
+      if (is_array($data) && isset($data['keterangan_absen'])) {
+        if ($data['keterangan_absen'] === 'Hadir') {
+          $data['tanggal_masuk'] = Carbon::now()->format('Y-m-d');
+          $data['waktu_masuk'] = Carbon::now()->format('H:i:s');
+        } else {
+          $data['lokasi_masuk'] = null;
+          $data['tanggal_masuk'] = null;
+          $data['waktu_masuk'] = null;
+        }
       }
     }
     return $data;
